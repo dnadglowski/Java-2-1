0efb062ba077de78ccae8159cb47ac0afb318cfe
Devin <dnadglowski@gmail.com>
2024-09-10 11:47:11 -0400

    Added comments

--- src/App.java
+++ src/App.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        //creating scanner
         Scanner scanner = new Scanner(System.in);
+        //print statement
         System.out.print("Enter a degree in Celsius: ");
 
         // Read an integer input from the user
         int number = scanner.nextInt();
+        //convert
         double fahrenheit = (9.0/5)* number + 32;
+        //print statement 
         System.out.print(number + " Celsius is " + fahrenheit +" Fahrenheit"); 
     }
 
